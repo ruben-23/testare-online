8dfa9b0f0e8708f05c3786c5f9bbcb35b66cfe87
Ruben <174733720+ruben-23@users.noreply.github.com>
2025-10-20 11:28:57 +0300

    Initial commit

---
+++ src/main/java/org/example/testareonline/AplicatieTestareOnlineApplication.java
@@ -0,0 +1,13 @@
+package org.example.testareonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicatieTestareOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicatieTestareOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/testareonline/AplicatieTestareOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testareonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicatieTestareOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
