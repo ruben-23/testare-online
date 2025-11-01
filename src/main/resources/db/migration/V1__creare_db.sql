bbdb25a2ce714a8f3508dac996313353ff858ac2
Ruben <174733720+ruben-23@users.noreply.github.com>
2025-11-01 21:55:49 +0200

    Creare baza de date

---
+++ src/main/resources/db/migration/V1__creare_db.sql
@@ -0,0 +1,103 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema test_app
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema test_app
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `test_app` DEFAULT CHARACTER SET utf8 ;
+USE `test_app` ;
+
+-- -----------------------------------------------------
+-- Table `test_app`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `test_app`.`users` (
+                                                  `id_user` INT NOT NULL AUTO_INCREMENT,
+                                                  `username` VARCHAR(50) NOT NULL,
+    `parola` VARCHAR(50) NOT NULL,
+    `rol` VARCHAR(50) NOT NULL,
+    PRIMARY KEY (`id_user`),
+    UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `test_app`.`domenii`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `test_app`.`domenii` (
+                                                    `id_domeniu` INT NOT NULL AUTO_INCREMENT,
+                                                    `nume` VARCHAR(50) NOT NULL,
+    PRIMARY KEY (`id_domeniu`))
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `test_app`.`teste`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `test_app`.`teste` (
+                                                  `id_test` INT NOT NULL AUTO_INCREMENT,
+                                                  `titlu` VARCHAR(50) NOT NULL,
+    `data_crearii` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `id_user` INT NOT NULL,
+    `id_domeniu` INT NOT NULL,
+    PRIMARY KEY (`id_test`),
+    INDEX `fk_teste_users_idx` (`id_user` ASC) VISIBLE,
+    INDEX `fk_teste_domenii1_idx` (`id_domeniu` ASC) VISIBLE,
+    CONSTRAINT `fk_teste_users`
+    FOREIGN KEY (`id_user`)
+    REFERENCES `test_app`.`users` (`id_user`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_teste_domenii1`
+    FOREIGN KEY (`id_domeniu`)
+    REFERENCES `test_app`.`domenii` (`id_domeniu`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `test_app`.`intrebari`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `test_app`.`intrebari` (
+                                                      `id_intrebare` INT NOT NULL AUTO_INCREMENT,
+                                                      `continut` VARCHAR(255) NOT NULL,
+    `id_test` INT NOT NULL,
+    PRIMARY KEY (`id_intrebare`),
+    INDEX `fk_intrebari_teste1_idx` (`id_test` ASC) VISIBLE,
+    CONSTRAINT `fk_intrebari_teste1`
+    FOREIGN KEY (`id_test`)
+    REFERENCES `test_app`.`teste` (`id_test`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `test_app`.`optiuni`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `test_app`.`optiuni` (
+                                                    `id_optiune` INT NOT NULL AUTO_INCREMENT,
+                                                    `id_intrebare` INT NOT NULL,
+                                                    `continut` VARCHAR(255) NOT NULL,
+    `punctaj` INT NOT NULL,
+    `is_correct` TINYINT NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id_optiune`),
+    INDEX `fk_optiuni_intrebari1_idx` (`id_intrebare` ASC) VISIBLE,
+    CONSTRAINT `fk_optiuni_intrebari1`
+    FOREIGN KEY (`id_intrebare`)
+    REFERENCES `test_app`.`intrebari` (`id_intrebare`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
